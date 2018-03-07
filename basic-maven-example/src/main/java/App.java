dc1d6bee88f75836c5121d377eb34dc408d67925
kaiwang <kaiwang@andrews-air-6.attlocal.net>
2018-03-07 16:40:43 -0500

    1st commit

---
+++ basic-maven-example/src/main/java/App.java
@@ -0,0 +1,16 @@
+/**
+ * Hello world!
+ * cahge a bit to see github push
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+	public int calculateSomething() {
+		return 0;
+	}
+}
